089993890604c860fc2ce2508d04b0f1dcdaf8eb
jcatillo <jcatillo1121@gmail.com>
2025-03-15 12:34:11 +0800

    feat: Added navbar

--- HospitalApp/HospitalApp/ViewModels/MainWindowViewModel.cs
+++ HospitalApp/HospitalApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace HospitalApp.ViewModels;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HospitalApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private bool _isPaneOpen = false;
+
+    [RelayCommand]
+    private void TriggerPane(){
+        IsPaneOpen = !IsPaneOpen;
+    }
 }
