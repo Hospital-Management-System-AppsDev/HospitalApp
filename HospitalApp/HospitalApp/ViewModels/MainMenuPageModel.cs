55d3e81aec0177bf3494d183177de1361a7793df
jcatillo <jcatillo1121@gmail.com>
2025-03-18 08:34:23 +0800

    Feat: fixed appointments

---
+++ HospitalApp/HospitalApp/ViewModels/MainMenuPageModel.cs
@@ -0,0 +1,6 @@
+namespace HospitalApp.ViewModels;
+
+public class MainContentPageModel:ViewModelBase
+{
+    
+}
---
+++ HospitalApp/HospitalApp/Views/MainMenuPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HospitalApp.Views;
+
+public partial class MainContentPageView : UserControl
+{
+    public MainContentPageView()
+    {
+        InitializeComponent();
+    }
+}
