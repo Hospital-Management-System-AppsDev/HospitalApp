4b1e9cfb281a8dc04637950a72643ee24cd0b5f1
Enya Moncayo <glitzkipalys@gmail.com>
2025-04-14 23:26:38 -0800

    feat: add basic models for appointments {not finish :D}

--- HospitalApp/HospitalApp/ViewModels/Admin/AppointmentsPageViewModel.cs
+++ HospitalApp/HospitalApp/ViewModels/Admin/AppointmentsPageViewModel.cs
@@ -1,6 +1,28 @@
-namespace HospitalApp.ViewModels;
+using System.Collections.ObjectModel;
 
-public class AppointmentsPageViewModel:ViewModelBase
+namespace HospitalApp.ViewModels
 {
-    
+    public class Appointment
+    {
+        public string PatientId { get; set; } = "";
+        public string PatientName { get; set; } = "";
+        public string TransactionType { get; set; } = "";
+        public string Doctor { get; set; } = "";
+        public string Status { get; set; } = "Pending";
+    }
+
+
+    public class AppointmentsPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<Appointment> Appointments { get; set; } = new()
+        {
+            new Appointment { PatientId = "00000000", PatientName = "Juan Dela Cruz", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Adamusa Pingay", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "Meoewmoew", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Theo Pondar", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "Nigs", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Enya Moncayo", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "Adam", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Jc Atillo", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "Michael", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Theo Pondar", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "Kem", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Enya Moncayo", Status = "Selected Option" },
+            new Appointment { PatientId = "00000000", PatientName = "DEez", TransactionType = "XXXXXXXXXX", Doctor = "Dr. Jc Atillo", Status = "Selected Option" }
+        };
+    }
 }
