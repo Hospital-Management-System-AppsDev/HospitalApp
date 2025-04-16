8212654bf31052faf1cd8e102f3ce19bb542ba69
jcatillo <jcatillo1121@gmail.com>
2025-04-16 09:18:13 +0800

    feat: Appointment Page (admin view) [TO BE FIXED: when new appointment is made, zero ang mogawas sa id ]

--- HospitalApp/HospitalApp/Views/AdminView/AppointmentsPageView.axaml.cs
+++ HospitalApp/HospitalApp/Views/AdminView/AppointmentsPageView.axaml.cs
@@ -1,13 +1,38 @@
+using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using HospitalApp.Models;
+using HospitalApp.ViewModels; // ✅ Add this!
+
 
 namespace HospitalApp.Views;
 
 public partial class AppointmentsPageView : UserControl
 {
-    public AppointmentsPageView()
+    public AppointmentsPageView() { 
+        InitializeComponent(); 
+        
+        this.Loaded += (sender, args) => 
+        {
+            if (DataContext is AppointmentsPageViewModel vm)
+            {
+                vm.ParentWindow = TopLevel.GetTopLevel(this) as Window;
+            }
+        };
+    }
+
+    public AppointmentsPageView(ApiService apiService, SignalRService signalRService)
     {
         InitializeComponent();
+        this.DataContext = new AppointmentsPageViewModel(apiService, signalRService);
+        this.Loaded += (sender, args) => 
+        {
+            if (DataContext is AppointmentsPageViewModel vm)
+            {
+                vm.ParentWindow = TopLevel.GetTopLevel(this) as Window;
+            }
+        };
+
     }
 }
