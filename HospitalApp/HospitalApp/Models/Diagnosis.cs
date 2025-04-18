12b78a321cac5c73f6a9a0ae5054bad3000fc463
jcatillo <jcatillo1121@gmail.com>
2025-04-18 14:17:00 +0800

    feat: AppointmentDetails implemented + PDF Generator + Email API [TODO] Overview and Medical Records

---
+++ HospitalApp/HospitalApp/Models/Diagnosis.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace HospitalApp.Models;
+
+public class Diagnosis
+{   
+    public string condition;
+    public string symptoms;
+    public string findings;
+    public string recommendations;
+}
