26acd9dadeb20a994c4aaf485794309e20ba4208
jcatillo <jcatillo1121@gmail.com>
2025-05-04 15:34:22 +0800

    feat: unfinished patient view

--- HospitalApp/HospitalApp/ViewModels/Admin/PatientPageViewModel.cs
+++ HospitalApp/HospitalApp/ViewModels/Admin/PatientPageViewModel.cs
@@ -1,14 +1,74 @@
 ﻿using System;
 using Avalonia;
+using System.Collections.ObjectModel;
+using HospitalApp.Models;
+using HospitalApp.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
 namespace HospitalApp.ViewModels;
 
-public partial class PatientPageViewModel:ViewModelBase
+public partial class PatientPageViewModel : ViewModelBase
 {
     private readonly ApiService _apiService;
     private readonly SignalRService _signalRService;
-    public PatientPageViewModel(ApiService _apiService, SignalRService _signalRService){
-        this._apiService = _apiService;
-        this._signalRService = _signalRService;
+
+    public ObservableCollection<Patient> Patients { get; } = new();
+
+    [ObservableProperty]
+    private Patient selectedPatient;
+
+    [ObservableProperty]
+    private bool showPatient;
+
+    [ObservableProperty]
+    private bool isEditing;
+
+    public PatientPageViewModel(ApiService apiService, SignalRService signalRService)
+    {
+        _apiService = apiService;
+        _signalRService = signalRService;
+        InitializeAsync();
+    }
+
+    public async void InitializeAsync()
+    {
+        await LoadDataAsync();
+    }
+
+    public async Task LoadDataAsync()
+    {
+        var patients = await _apiService.GetPatientsAsync();
+        Patients.Clear();
+        foreach (var patient in patients)
+        {
+            Patients.Add(patient);
+        }
+    }
+
+    partial void OnSelectedPatientChanged(Patient? value)
+    {
+        ShowPatient = value != null;
+        IsEditing = false;
+    }
+
+    [RelayCommand]
+    private void EditButton()
+    {
+        IsEditing = !IsEditing;
+    }
+
+    [RelayCommand]
+    private void SaveChanges()
+    {
+        IsEditing = false;
+    }
+
+    [RelayCommand]
+    private void DeletePatient()
+    {
+        IsEditing = false;
     }
 }
+
